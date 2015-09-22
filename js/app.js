9e81b4c739f92c30b6226f00a9172fe47f38c587
Claire Annan <claire.annan.sme@sonymusic.com>
2015-09-22 14:12:21 -0400

    Adding cool stance and sounds. Added header with logos.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,45 @@ function playHadouken() {
   $('#hadouken-sound')[0].play();
 }
 
+function playBackground() {
+	$('#bg-sound')[0].volume = 0.3;
+  $('#bg-sound')[0].load();
+  $('#bg-sound')[0].play();
+}
+
+function stopBackground() {
+	$('#bg-sound')[0].pause();
+}
+
+function playCool() {
+	$('#cool-sound')[0].volume = 0.5;
+  $('#cool-sound')[0].load();
+  $('#cool-sound')[0].play();
+}
+
+function stopPlayCool() {
+	$('#cool-sound')[0].pause();
+}
+
+
+function showInstructions() {
+	$('.instructions p:first-child').fadeIn(1000, function() {
+		$(this).next().fadeIn(2000, function() {
+			$(this).next().fadeIn(2000);
+		});
+	});
+}
+
+function hideInstructions() {
+	var word = '<img class="word" src="images/word.png"/>';
+	$('.instructions').children().hide();
+	$(word).appendTo('.instructions');
+}
+
 $(document).ready(function(){
+	playBackground();
+	showInstructions();
+
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
@@ -13,14 +51,30 @@ $(document).ready(function(){
 		$('.ryu-ready').hide();	
 	}).mousedown(function(){
 		playHadouken();
+		hideInstructions();		
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate({'left': '1020px'}, 'slow', function(){
 			$(this).hide();
 			$(this).css('left', '520px');
+			$('.instructions p').fadeIn('slow');
+			$('.word').remove();
 		});
 	}).mouseup(function(){
 		$('.ryu-ready').show();
 		$('.ryu-throwing').hide();
+	});
+
+	$(document).keydown(function(e) {
+		if(e.which === 88){
+			$('.ryu-cool').show().siblings().hide();
+			playCool();
+			stopBackground();
+		}
+	}).keyup(function() {
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();
+		playBackground();
+		stopPlayCool();
 	});
 });
